26180f3fcbce631bf94e10dc04e77b7afa82f148
juniuszhou <junius.zhou@gmail.com>
2025-03-19 09:49:44 +0800

    save more files

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,15 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {},
+    localhost: {},
+    customNetwork: {
+      url: "https://your-custom-network-url.com", // Replace with your network's RPC URL
+      chainId: 1234, // Optional: specify the chain ID of your custom network
+    },
+    // You can add more networks here
+  },
 };
 
 export default config;
