4778e9713b3cf00d9965ee4de4fdbf293608dd07
juniuszhou <junius.zhou@gmail.com>
2025-03-24 18:47:32 +0800

    ts revive

---
+++ hardhat-revive-ts/test/Storage.js
@@ -0,0 +1,23 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Storage", function () {
+  let storage;
+  const initialValue = 42;
+
+  beforeEach(async function () {
+    const Storage = await ethers.getContractFactory("Storage");
+    storage = await Storage.deploy(initialValue);
+    await storage.waitForDeployment();
+  });
+
+  it("Should return the initial value", async function () {
+    expect(await storage.storedNumber()).to.equal(initialValue);
+  });
+
+  it("Should set new value", async function () {
+    const newValue = 100;
+    await storage.setNumber(newValue);
+    expect(await storage.storedNumber()).to.equal(newValue);
+  });
+});
